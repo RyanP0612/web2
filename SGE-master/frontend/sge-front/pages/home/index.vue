e6ba6f2a920bca6cc783106c6305b419a42281b1
RyanP0612 <ryansalva06112@gmail.com>
2024-03-21 20:21:21 -0300

    sgeCommit

---
+++ SGE-master/frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+const {signOut} = useAuth();
+
+definePageMeta({
+    middleware: 'auth'
+})
+
+const sair = ()=>{
+    signOut({redirect: false}).then(()=>{
+            navigateTo('/');
+        })
+}
+
+</script>
+
+<template>
+    <div>
+        <h1>Minha Tela Home!!!</h1>    
+        <button @click="sair" class="customButton">SAIR</button>
+    </div>
+</template>
+
+<style lang="scss">
+
+</style>
